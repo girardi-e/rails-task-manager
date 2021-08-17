b1315033caf1b6503eaa928ac8389a900b1ac6ad
Emilio Girardi <64197141+girardi-e@users.noreply.github.com>
2021-08-17 20:13:10 +0200

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # post 'tasks', to: 'tasks#create'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+  resources :tasks
 end
